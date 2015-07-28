dd538d4f0509e19df8a8b8a291d6d4c3a57e6769
nwittstruck <nwittstruck@users.noreply.github.com>
2015-07-28 13:22:22 +0200

    create method

--- app/controllers/spree/api/food_information_controller.rb
+++ app/controllers/spree/api/food_information_controller.rb
@@ -1,23 +1,56 @@
-class Spree::Api::FoodInformationController < Spree::Api::BaseController
-  before_action :find_food_information, except: [:index]
+module Spree
+  module Api
 
-  def index
-    authorize! :read, Spree::FoodInformation
-    @food_informations = Spree::FoodInformation.accessible_by(current_ability, :read).all().
-          ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+    class FoodInformationController < Spree::Api::BaseController
+      before_action :find_food_information, except: [:index, :create]
 
-    respond_with(@food_informations)
-  end
+      def index
+        authorize! :read, FoodInformation
+        @food_informations = FoodInformation.accessible_by(current_ability, :read).all().
+            ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
 
-  def show
-    authorize! :read, Spree::FoodInformation
-    respond_with(@food_information)
-  end
+        respond_with(@food_informations)
+      end
 
-  private
-  def find_food_information
-    # authorize request here as well:
-    authorize! :read, Spree::FoodInformation
-    @food_information = Spree::FoodInformation.find(params[:id])
+      def show
+        authorize! :read, FoodInformation
+        respond_with(@food_information)
+      end
+
+      def create
+        authorize! :create, FoodInformation
+        @food_information = FoodInformation.new(food_information_params)
+        if @food_information.save
+          respond_with(@food_information, status: 201, default_template: :show)
+        else
+          invalid_resource!(@food_information)
+        end
+      end
+
+      private
+      def find_food_information
+        # authorize request here as well:
+        authorize! :read, FoodInformation
+        @food_information = FoodInformation.find(params[:id])
+      end
+
+      def food_information_params
+        params.require(:food_information).permit(permitted_food_information_attributes)
+      end
+
+      def permitted_food_information_attributes
+        [:description,
+         :net_contents,
+         :origin,
+         :producer_address,
+         :allergens,
+        :ingredients => [],
+         :nutrition_information => [[:quantity, :nutrition_values => [:nutrition, :name, :value]]],
+        ]
+      end
+    end
   end
 end
+
+#{:quantity => "100g", :nutrition_values => [{:nutrition => "energy", :name => "Energy kJ / kCal", :value => "726 kJ / 173 kcal"},
+#                                            {:nutrition => "fat", :name => "Fat", :value => "9,0g"}]
--- spec/controllers/spree/api/food_information_controller_spec.rb
+++ spec/controllers/spree/api/food_information_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Spree::Api::FoodInformationController, type: :controller do
   render_views
 
-  let(:food_information) {create(:food_information)}
+  let(:food_information) { create(:food_information) }
 
 
   # this will add an api key to the request:
@@ -39,9 +39,40 @@ RSpec.describe Spree::Api::FoodInformationController, type: :controller do
 
     describe "GET #show" do
       it "returns access denied" do
-        api_get :show, {:id => food_information.id}
+        api_get :show, {id: food_information.id}
         expect(response).to have_http_status(:success)
         expect(json_response[:description]).to eq(food_information.description)
+      end
+    end
+
+
+    describe "POST #create" do
+      let(:food_information_data) { {
+          :description => "Description of the product",
+          :ingredients => ["Flour", "Sugar", "Hazelnuts"],
+          :net_contents => "250g",
+          :origin => "Germany",
+          :producer_address => "Streetnumber 123, 20202 Example City,Germany",
+          :nutrition_information => [{:quantity => "100g", :nutrition_values => [{:nutrition => "energy", :name => "Energy kJ / kCal", :value => "726 kJ / 173 kcal"},
+                                                                                 {:nutrition => "fat", :name => "Fat", :value => "9,0g"}]},
+                                     {:quantity => "Serving", :nutrition_values => [{:nutrition => "energy", :name => "Energy kJ / kCal", :value => "126 kJ / 73 kcal"},
+                                                                                    {:nutrition => "fat", :name => "Fat", :value => "1,0g"}]}
+          ],
+          :allergens => "Hazelnuts"
+      } }
+
+      it "returns access denied" do
+        api_post :create, {food_information: food_information_data}
+        expect(response).to have_http_status(:success)
+
+        expect(json_response[:id]).not_to be_nil
+        expect(json_response[:description]).to eq(food_information_data[:description])
+        expect(json_response[:ingredients]).to eq(food_information_data[:ingredients])
+        expect(json_response[:net_contents]).to eq(food_information_data[:net_contents])
+        expect(json_response[:origin]).to eq(food_information_data[:origin])
+        expect(json_response[:producer_address]).to eq(food_information_data[:producer_address])
+        expect(json_response[:nutrition_information]).to eq(food_information_data[:nutrition_information])
+        expect(json_response[:allergens]).to eq(food_information_data[:allergens])
       end
     end
   end
